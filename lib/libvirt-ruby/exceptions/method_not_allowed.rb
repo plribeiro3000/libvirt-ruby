ba955298070f873fd5bfa3ca9fca77e07be713e5
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2012-02-22 23:03:32 -0200

    Removing Unused Files

--- lib/libvirt-ruby/exceptions/method_not_allowed.rb
+++
@@ -1,7 +0,0 @@
-module Exceptions
-  class MethodNotAllowed < StandardError
-    def message
-      "This method should not be called direct on this module"
-    end
-  end
-end
--- lib/libvirt-ruby/util.rb
+++ lib/libvirt-ruby/util.rb
@@ -2,7 +2,6 @@ module Libvirt
   module Ruby
     module Util
       def dispatcher(method, args = [])
-        raise Exceptions::MethodNotAllowed if self.instance_of?(Libvirt::Ruby::Util)
         return_type = args.delete(args.last)
         attach_function (self.klass + method.to_s), (self.klass + method.to_s), args, return_type
         send((self.klass + method.to_s), args)
